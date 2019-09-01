f0a5e5fed3223a5b73c43de8fdc0e3f6a16fde05
ikx <ihsankx@gmail.com>
2019-09-01 13:04:46 +0100

    put the gsmr in a fuc
    ]
    tion

--- main.py
+++ main.py
@@ -1,35 +1,43 @@
 import random
 import time
 
-attempts = 0
-max_attempts = 5
-guess = -1
-number = random.randint(0, 100)
-correct = False
+def play_number_game():
+    attempts = 0
+    max_attempts = 5
+    guess = -1
+    number = random.randint(0, 100)
+    correct = False
 
-while number != guess and attempts < max_attempts:
-    rawInput = input("guess: ")
-    try:
-        guess = int(rawInput)
-    except:
-        print("EROOR")
-        continue
-    attempts = attempts + 1
-    if guess < number:
-        print("too small")
-    elif guess > number:
-        print("too big")
-    else:
-        correct = True
-        print("right!")
+    while number != guess and attempts < max_attempts:
+        attempts = attempts + 1
+        rawInput = input("guess: ")
+        try:
+            guess = int(rawInput)
+        except:
+            print("EROOR")
+            continue
+        if guess < number:
+            print("too small")
+        elif guess > number:
+            print("too big")
+        else:
+            correct = True
+            print("right!")
 
-if not correct:
-    print("F")
-    time.sleep(1)
-    print("A")
-    time.sleep(1)
-    print("I")
-    time.sleep(1)
-    print("L")
-    time.sleep(1)
-    print("the correct number was " + str(number))
+
+    message = "FAIL"
+
+    if not correct:
+        for letter in message:
+            print(letter)
+            time.sleep(1)
+        print("the correct number was " + str(number))
+
+    warialbe = "quote"
+    variavle = 'quote'
+
+
+play = "yes"
+while play == "yes":
+    play_number_game()
+    play = input("do u want to play again?").lower()
